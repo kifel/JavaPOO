3eedb734e6991c667491e766be0497bee374f4d5
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-08-31 10:54:41 -0300

    Update try

--- src/br/com/residencia/poo/aula1/SegundoExercicio.java
+++ src/br/com/residencia/poo/aula1/SegundoExercicio.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class SegundoExercicio {
     public static void main(String[] args) {
         // A try-with-resources statement.
+        System.out.println("Digite o seu nome");
         try (Scanner myObj = new Scanner(System.in)) {
-            System.out.println("Digite o seu nome");
             String name = myObj.nextLine(); // Getting the user input and storing it in the variable `name`.
             System.out.println("Olá O seu nome é " + name);
         }
