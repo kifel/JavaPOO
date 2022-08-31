121f0ee3004b01cb6f4eb6a63337fdedfe88ac4d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-08-31 12:09:44 -0300

    adding comments

--- src/br/com/residencia/poo/lista1/PrimeiroExercicio.java
+++ src/br/com/residencia/poo/lista1/PrimeiroExercicio.java
@@ -4,9 +4,12 @@ import javax.swing.JOptionPane;
 
 public class PrimeiroExercicio {
     public static void main(String[] args) {
+        // It's asking the user to input a name.
         String name = JOptionPane.showInputDialog("Digite o seu nome");
+        // It's asking the user to input a surname.
         String sobrenome = JOptionPane.showInputDialog("Digite o seu sobrenome");
 
+        // It's showing a message dialog with the name and surname.
         JOptionPane.showMessageDialog(null, "Bem vindo(a) "+ name + " " + sobrenome);
     }
 }
