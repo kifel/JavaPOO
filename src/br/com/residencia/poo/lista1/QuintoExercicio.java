bc2fc7f74b80fa4fb34c19fecea0f7177d7f81f0
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-08-31 17:30:52 -0300

    Exercicio 5 completo

--- src/br/com/residencia/poo/lista1/QuintoExercicio.java
+++ src/br/com/residencia/poo/lista1/QuintoExercicio.java
@@ -2,6 +2,8 @@ package br.com.residencia.poo.lista1;
 
 public class QuintoExercicio {
     public static void main(String[] args) {
-        
-    }
+        System.out.println("O resultado da expressão é " + (2 + 3 - (5 * 8) - 4 + 354 - 521 + (7 * 66)));
+        System.out.println("O resultado da expressão é " + (2 + 7 * (14-21) + 28 * 3 * 42 + 740 - (156 + 4 + 40) * 9));
+
+    } 
 }
