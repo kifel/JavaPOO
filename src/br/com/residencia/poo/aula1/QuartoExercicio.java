d6a7f02cf64597844097e59b582e7fedcacad426
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-08-31 11:27:33 -0300

    Update exercicio 4

--- src/br/com/residencia/poo/aula1/QuartoExercicio.java
+++ src/br/com/residencia/poo/aula1/QuartoExercicio.java
@@ -8,11 +8,15 @@ public class QuartoExercicio {
         String name = JOptionPane.showInputDialog("Nome do aluno");
         Double soma = 0.0;
 
+        // It's a for loop that will run 3 times.
         for (int i = 0; i < 3; i++) {
+            // It's asking the user to input a number.
             String num = JOptionPane.showInputDialog("Digite a nota " + (i + 1) + " ");
 
+            // It's converting the string to a double.
             Double nota = Double.parseDouble(num);
 
+            // It's adding the value of `nota` to `soma`.
             soma += nota;
         }
 
