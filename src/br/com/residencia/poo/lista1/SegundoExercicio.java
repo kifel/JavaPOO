d0082a80a6e888f7f1f7ba17c25af5e9a5d0e1b3
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-08-31 12:47:04 -0300

    adding comments

--- src/br/com/residencia/poo/lista1/SegundoExercicio.java
+++ src/br/com/residencia/poo/lista1/SegundoExercicio.java
@@ -8,12 +8,16 @@ public class SegundoExercicio {
         int sum = 0;
         
         try(Scanner myObj = new Scanner(System.in)) {
+            // Asking for the age of 6 friends and adding it to the sum variable.
             for (int i = 0; i < 6; i++) {
                 System.out.println("Digite a idade do amigo " + (i+1));
+                // It's asking for the user input and storing it in the `age` variable.
                 int age = myObj.nextInt();
+                // It's adding the value of `age` to the value of `sum` and storing the result in `sum`.
                 sum += age;
             }
             System.out.println("Digite a sua idade");
+            // It's asking for the user input and storing it in the `age` variable.
             int age = myObj.nextInt();
             System.out.println("Tempo de vida " + (age + sum));
         }
