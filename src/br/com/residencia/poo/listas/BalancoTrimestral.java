f8653bcf8e1ea92f71beec661750576fe17bffdb
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-02 16:40:18 -0300

    Novo exercicio

---
+++ src/br/com/residencia/poo/listas/BalancoTrimestral.java
@@ -0,0 +1,13 @@
+package br.com.residencia.poo.listas;
+
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+        
+        int gastosJaneiro = 1500;
+        int gastosFevereiro = 2300;
+        int gastosMarco = 1700;
+        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        System.out.println(gastosTrimestre);
+    }
+}
