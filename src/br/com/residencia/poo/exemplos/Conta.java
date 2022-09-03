770455ddedf0d6597add622f787692163bdde852
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-02 21:58:54 -0300

    saldo fixed

--- src/br/com/residencia/poo/exemplos/Conta.java
+++ src/br/com/residencia/poo/exemplos/Conta.java
@@ -5,8 +5,14 @@ public class Conta {
     String titular;
     double saldo;
 
-    public void sacar(double valor) {
-        double novoSaldo = this.saldo - valor;
-        this.saldo = novoSaldo;
+    public boolean sacar(double valor) {
+        if (this.saldo < valor) {
+            return false;
+        }else {
+            double novoSaldo = this.saldo - valor;
+            this.saldo = novoSaldo;
+
+            return true;
+        }
     }
 }
