b254bbb4c19e312fc3d10ff71cb49e20151de77d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-05 20:45:27 -0300

    Fortamação de codigo

--- bancoserratec/src/br/com/poo/principal/App.java
+++ bancoserratec/src/br/com/poo/principal/App.java
@@ -7,10 +7,10 @@ public class App {
         Conta minhaConta1 = new Conta(123, "kifel", 1000);
         Conta minhaConta2 = new Conta(1234, "Teste", 0);
         minhaConta1.sacar(100);
-         minhaConta1.depositar(3000);
+        minhaConta1.depositar(3000);
         System.out.println("\n");
         System.out.println("\n");
-        minhaConta1.transferir(150.0, minhaConta2);
+        minhaConta1.transferir(1000.0, minhaConta2);
         System.out.println("\n");
 
         System.out.println("Titular da conta:" + minhaConta1.getTitular() + "\nSaldo da conta:" + minhaConta1.getSaldo()
