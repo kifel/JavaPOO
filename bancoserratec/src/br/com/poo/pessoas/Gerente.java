74b7fe18ccdfb41da7b32fcd2bbdcc9b46f5fbb9
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-08 22:45:48 -0300

    improving the comment

--- bancoserratec/src/br/com/poo/pessoas/Gerente.java
+++ bancoserratec/src/br/com/poo/pessoas/Gerente.java
@@ -3,7 +3,7 @@ package br.com.poo.pessoas;
 public class Gerente extends Funcionario{
     
 
-    //"If the employee is a manager, return the manager's bonus, otherwise return the employee's bonus."
+    //"If the employee is a manager, return the manager's bonus, witch is 15% of the salary, otherwise return the employee's bonus."
     @Override
     public double getBonificacao() {
         return this.salario * 0.15;
